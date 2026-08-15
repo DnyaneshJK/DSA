90d54ac52189e02020fdaa0ca33d575157817f1a
Dnyanesh Khandare <dnyaneshkaij@gmail.com>
2026-08-15 15:18:45 +0530

    Time: 1 ms (99.94%) | Memory: 47.2 MB (6.96%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,17 +1,25 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer,Integer> map  = new HashMap<>();
-        int ans=0;
-         
-         for(int num : nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-         }
 
-         for(int n : map.keySet()){
-            if(map.get(n)==1){
-                ans=n;
-            }
-         }
+        int ans=0;
+
+        for(int n : nums){
+            ans = ans^n;
+        }
+
         return ans;
+        // HashMap<Integer,Integer> map  = new HashMap<>();
+        // int ans=0;
+         
+        //  for(int num : nums){
+        //     map.put(num,map.getOrDefault(num,0)+1);
+        //  }
+
+        //  for(int n : map.keySet()){
+        //     if(map.get(n)==1){
+        //         ans=n;
+        //     }
+        //  }
+        // return ans;
     }
 }
