58e3a968ca3af3e7102ba4832fe9a06193f6f5c9
Dnyanesh Khandare <dnyaneshkaij@gmail.com>
2026-08-30 13:07:00 +0530

    Time: 26 ms (88.57%) | Memory: 55.1 MB (28.54%) - LeetSync

--- 937-online-stock-span/online-stock-span.java
+++ 937-online-stock-span/online-stock-span.java
@@ -1,25 +1,47 @@
 class StockSpanner {
 
-    ArrayList<Integer> arr;
+    Deque<int[]> stack;
+
     public StockSpanner() {
-        arr = new ArrayList<>();
+        stack = new ArrayDeque<>();
+    }
+    
+    public int next(int price) {
+
+        int s =1;
+
+        while(!stack.isEmpty() && price>=stack.peek()[0]){
+            s += stack.pop()[1];
+        }
+        stack.push(new int[]{price,s});
+
+        return s;
         
     }
-    public int next(int price) {
-        int c=1;
-        int i=arr.size()-1;
-        while(i>=0){
-            if(price>=arr.get(i)) {
-                c++;
-                i--;
-            } else{
-                break;
-            }
-        }
-        arr.add(price);
-        return c;
-    }
 }
+
+
+
+//  ArrayList<Integer> arr;
+//     public StockSpanner() {
+//         arr = new ArrayList<>();
+        
+//     }
+//     public int next(int price) {
+//         int c=1;
+//         int i=arr.size()-1;
+//         while(i>=0){
+//             if(price>=arr.get(i)) {
+//                 c++;
+//                 i--;
+//             } else{
+//                 break;
+//             }
+//         }
+//         arr.add(price);
+//         return c;
+//     }
+
 
 /**
  * Your StockSpanner object will be instantiated and called as such:
