73a51ea193201da75313a3ffac6c9865450add67
Dnyanesh Khandare <dnyaneshkaij@gmail.com>
2026-08-30 12:51:31 +0530

    Time: 596 ms (5.00%) | Memory: 53.4 MB (92.88%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.java
@@ -0,0 +1,30 @@
+class StockSpanner {
+
+    ArrayList<Integer> arr;
+    Deque<Integer> stack;
+
+    public StockSpanner() {
+        arr = new ArrayList<>();
+        stack = new ArrayDeque<>();
+    }
+    public int next(int price) {
+        int c=1;
+        int i=arr.size()-1;
+        while(i>=0){
+            if(price>=arr.get(i)) {
+                c++;
+                i--;
+            } else{
+                break;
+            }
+        }
+        arr.add(price);
+        return c;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
