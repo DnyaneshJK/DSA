e93f098681fc9ae183de7cc1b90a9d62c9a382fa
Dnyanesh Khandare <dnyaneshkaij@gmail.com>
2026-09-02 10:52:52 +0530

    Time: 38 ms (84.97%) | Memory: 80.2 MB (21.45%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -7,7 +7,7 @@ class Solution {
         
         for(int i=0;i<n;i++){
 
-            while(!s.isEmpty() && arr[s.peek()] > arr[i]){
+            while(!s.isEmpty() && arr[s.peek()] >= arr[i]){
                 s.pop();
             }
 
