249cf623f8159e47ea2f10380a67295beae48677
Dnyanesh Khandare <dnyaneshkaij@gmail.com>
2026-09-03 23:40:43 +0530

    Time: 3 ms (27.34%) | Memory: 43.4 MB (90.83%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+
+        Deque<Character> stack = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            
+            if(stack.contains(c)) continue;
+            while(!stack.isEmpty() && c < stack.peek() && s.indexOf(stack.peek(),i+1) != -1 ) {
+               stack.pop();
+            }
+
+            stack.push(c);
+            
+        }
+
+        while(!stack.isEmpty()){
+            char c = stack.removeLast();
+            sb.append(c);
+        }
+
+        return sb.toString();
+        
+    }
+}
