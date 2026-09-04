025258f74f65fb3af811f9c1d68d0517d6bfb2a3
Dnyanesh Khandare <dnyaneshkaij@gmail.com>
2026-09-04 19:13:12 +0530

    Time: 3 ms (27.25%) | Memory: 43.5 MB (84.50%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -4,23 +4,23 @@ class Solution {
         Deque<Character> stack = new ArrayDeque<>();
         StringBuilder sb = new StringBuilder();
 
-        for(int i=0;i<s.length();i++){
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            
-            if(stack.contains(c)) continue;
-            while(!stack.isEmpty() && c < stack.peek() && s.indexOf(stack.peek(),i+1) != -1 ) {
-               stack.pop();
+            if (stack.contains(c))
+                continue;
+            while (!stack.isEmpty() && c < stack.peek() && s.indexOf(stack.peek(), i + 1) != -1) {
+                stack.pop();
             }
 
             stack.push(c);
-            
+
         }
 
-        while(!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             sb.append(stack.removeLast());
         }
 
         return sb.toString();
-        
+
     }
 }
