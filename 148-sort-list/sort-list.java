7733d3f4e103f142d232f2ee00033359e4548cab
Dnyanesh Khandare <dnyaneshkaij@gmail.com>
2026-09-15 12:06:23 +0530

    Time: 10 ms (49.26%) | Memory: 58.5 MB (99.78%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -9,20 +9,64 @@
  * }
  */
 class Solution {
-    public ListNode sortList(ListNode head) {
+
+    public ListNode merge(ListNode l1,ListNode l2){
         ListNode dummy = new ListNode(0);
         ListNode curr = dummy;
-        ArrayList<Integer> al = new ArrayList<>();
-        while(head!=null){
-            al.add(head.val);
-            head=head.next;
+        while(l1!=null && l2!=null){
+            if(l1.val<=l2.val){
+                curr.next=l1;
+                l1=l1.next;
+            } else{
+                curr.next=l2;
+                l2=l2.next;
+            }
+             curr=curr.next;
         }
-        Collections.sort(al);
 
-        for(int num : al){
-            curr.next = new ListNode(num);
-            curr=curr.next;
+
+        if(l1!=null){
+            curr.next=l1;
+        } else{
+            curr.next=l2;
         }
         return dummy.next;
     }
+
+
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode fast = head;
+        ListNode slow=head;
+        ListNode prev = null;
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        if(prev!=null) prev.next=null;
+        head = sortList(head);
+        slow = sortList(slow);
+        ListNode ans = merge(head,slow);
+        return ans;
+    }
 }
+
+// class Solution {
+//     public ListNode sortList(ListNode head) {
+//         ListNode dummy = new ListNode(0);
+//         ListNode curr = dummy;
+//         ArrayList<Integer> al = new ArrayList<>();
+//         while(head!=null){
+//             al.add(head.val);
+//             head=head.next;
+//         }
+//         Collections.sort(al);
+
+//         for(int num : al){
+//             curr.next = new ListNode(num);
+//             curr=curr.next;
+//         }
+//         return dummy.next;
+//     }
+// }
